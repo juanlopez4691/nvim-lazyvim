e4178a03f7a3967a50436666cac03dc7743c7d26
Joan López <juanlopez4691@gmail.com>
2024-12-27 09:33:29 +0100

    refactor: keymaps assignments

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,27 +2,29 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local keymap = vim.keymap
+
 -- Remove original keymaps
-vim.keymap.del("n", "<C-Up>")
-vim.keymap.del("n", "<C-Down>")
-vim.keymap.del("n", "<C-Left>")
-vim.keymap.del("n", "<C-Right>")
+keymap.del("n", "<C-Up>")
+keymap.del("n", "<C-Down>")
+keymap.del("n", "<C-Left>")
+keymap.del("n", "<C-Right>")
 
 -- Resize window splits
 local split = require("helpers.split")
 
-vim.keymap.set("n", "<A-right>", function()
+keymap.set("n", "<A-right>", function()
   split.resize("right")
 end, { silent = true })
 
-vim.keymap.set("n", "<A-left>", function()
+keymap.set("n", "<A-left>", function()
   split.resize("left")
 end, { silent = true })
 
-vim.keymap.set("n", "<A-down>", function()
+keymap.set("n", "<A-down>", function()
   split.resize("down")
 end, { silent = true })
 
-vim.keymap.set("n", "<A-up>", function()
+keymap.set("n", "<A-up>", function()
   split.resize("up")
 end, { silent = true })
