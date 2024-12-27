6e364ee55ab2d9734ad92cde7803aaf7e0c85c41
Joan López <juanlopez4691@gmail.com>
2024-12-27 08:46:58 +0100

    feat: adjust autocompletion menu appearence

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -7,11 +7,63 @@ return {
       ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
     },
     completion = {
+      ghost_text = {
+        enabled = true,
+      },
       list = {
         selection = "preselect",
       },
       menu = {
+        min_width = 25,
         border = "rounded",
+        draw = {
+          columns = {
+            { "kind_icon" },
+            { "label", "label_description" },
+            { "kind" },
+            { "source_name" },
+          },
+          components = {
+            kind_icon = {
+              ellipsis = false,
+              text = function(ctx)
+                return ctx.kind_icon .. ctx.icon_gap
+              end,
+              highlight = function(ctx)
+                return require("blink.cmp.completion.windows.render.tailwind").get_hl(ctx) or "BlinkCmpKind" .. ctx.kind
+              end,
+            },
+            label = {
+              width = { fill = true, max = 60 },
+              text = function(ctx)
+                return ctx.label .. ctx.label_detail
+              end,
+            },
+            label_description = {
+              width = { fill = true, max = 60 },
+              text = function(ctx)
+                return "    " .. ctx.label_description
+              end,
+              highlight = "BlinkCmpLabelDescription",
+            },
+            kind = {
+              ellipsis = false,
+              width = { fill = true },
+              text = function(ctx)
+                return ctx.kind
+              end,
+              highlight = function(ctx)
+                return require("blink.cmp.completion.windows.render.tailwind").get_hl(ctx) or "BlinkCmpKind" .. ctx.kind
+              end,
+            },
+
+            source_name = {
+              text = function(ctx)
+                return "[" .. ctx.source_name .. "]"
+              end,
+            },
+          },
+        },
       },
       documentation = {
         window = {
