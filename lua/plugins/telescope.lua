00d7ee101892571319b91627f41297a1f22af8b8
Joan López <juanlopez4691@gmail.com>
2024-12-28 11:38:47 +0100

    refactor: load telescope extensions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,48 +1,59 @@
 return {
-  { "nvim-telescope/telescope-project.nvim" },
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = {
-      defaults = {
-        layout_config = {
-          height = 0.95,
-          width = 0.95,
-        },
-        mappings = {
-          i = {
-            ["<C-j>"] = require("telescope.actions").move_selection_next,
-            ["<C-k>"] = require("telescope.actions").move_selection_previous,
-            ["<C-h>"] = require("telescope.actions").preview_scrolling_left,
-            ["<C-l>"] = require("telescope.actions").preview_scrolling_right,
-          },
-          n = {
-            ["j"] = require("telescope.actions").move_selection_next,
-            ["k"] = require("telescope.actions").move_selection_previous,
-            -- Add more custom normal mode mappings here
-          },
-        },
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      layout_config = {
+        height = 0.95,
+        width = 0.95,
       },
-    },
-    extensions = {
-      project = {
-        theme = "dropdown",
-      },
-    },
-    keys = {
-      {
-        "<leader>fp",
-        function()
-          require("telescope").extensions.project.project({
-            display_type = "full",
-            theme = "dropdown",
-          })
-        end,
-        desc = "Find project",
+      mappings = {
+        i = {
+          ["<C-j>"] = require("telescope.actions").move_selection_next,
+          ["<C-k>"] = require("telescope.actions").move_selection_previous,
+          ["<C-h>"] = require("telescope.actions").preview_scrolling_left,
+          ["<C-l>"] = require("telescope.actions").preview_scrolling_right,
+        },
+        n = {
+          ["j"] = require("telescope.actions").move_selection_next,
+          ["k"] = require("telescope.actions").move_selection_previous,
+          -- Add more custom normal mode mappings here
+        },
       },
     },
   },
+  extensions = {
+    project = {
+      theme = "dropdown",
+    },
+  },
+  keys = {
+    {
+      "<leader>fp",
+      function()
+        require("telescope").extensions.project.project({
+          display_type = "full",
+          theme = "dropdown",
+        })
+      end,
+      desc = "Find project",
+    },
+  },
   dependencies = {
-    { "nvim-telescope/telescope-project.nvim" },
-    { "nvim-telescope/telescope-file-browser.nvim" },
+    {
+      "nvim-telescope/telescope-project.nvim",
+      config = function()
+        LazyVim.on_load("telescope", function()
+          require("telescope").load_extension("project")
+        end)
+      end,
+    },
+    {
+      "nvim-telescope/telescope-file-browser.nvim",
+      config = function()
+        LazyVim.on_load("telescope", function()
+          require("telescope").load_extension("file_browser")
+        end)
+      end,
+    },
   },
 }
