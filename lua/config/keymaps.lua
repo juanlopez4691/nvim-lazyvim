804d48bb90e92e307437e1a523e10053e951a0ed
Joan López <juanlopez4691@gmail.com>
2024-12-29 20:28:48 +0100

    feat: modifie keymaps to open a terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -94,3 +94,28 @@ wk.add({
     icon = { icon = "", color = "yellow" },
   },
 })
+
+-- Terminal
+-- Remove some original keymaps
+keymap.del("n", "<leader>fT")
+keymap.del("n", "<leader>ft")
+
+wk.add({
+  { mode = "n", "<leader>t", group = "Terminal" },
+  {
+    mode = "n",
+    "<leader>tt",
+    function()
+      Snacks.terminal()
+    end,
+    desc = "Terminal (cwd)",
+  },
+  {
+    mode = "n",
+    "<leader>tT",
+    function()
+      Snacks.terminal(nil, { cwd = LazyVim.root() })
+    end,
+    desc = "Terminal (Root Dir)",
+  },
+})
