6e30de7a12f854f79e202a9782b78a7a30041920
Joan López <juanlopez4691@gmail.com>
2024-12-30 23:03:25 +0100

    feat: customize treesitter to support blade templates

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,4 +10,36 @@ return {
       "vue",
     },
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "blade",
+        "css",
+        "php",
+        "php_only",
+        "phpdoc",
+      },
+    },
+    config = function(_, opts)
+      ---@class ParserInfo
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        filetype = "blade",
+      }
+
+      vim.filetype.add({
+        pattern = {
+          [".*%.blade%.php"] = "blade",
+        },
+      })
+
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
 }
