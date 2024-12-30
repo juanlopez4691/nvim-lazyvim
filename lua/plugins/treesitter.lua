2a5bf3f6554e404ae277fbb6ee985e75093b217b
Joan López <juanlopez4691@gmail.com>
2024-12-30 18:43:37 +0100

    feat: customize nvim-treesitter config

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "css",
+      "http",
+      "markdown",
+      "php",
+      "phpdoc",
+      "vue",
+    },
+  },
+}
