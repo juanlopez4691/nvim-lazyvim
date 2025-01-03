dfd1f87841392322a55cc8948b2313fb32941069
Joan López <juanlopez4691@gmail.com>
2025-01-03 13:30:01 +0100

    feat: customize cattpuccin to improve visibility of copilot suggestions

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,8 +3,11 @@ return {
   lazy = true,
   opts = {
     style = "night",
-    on_highlights = function(hl)
-      hl.WinSeparator = { fg = "#4b6f9e" }
+    dim_inactive = true,
+    lualine_bold = true,
+    on_highlights = function(hl, colors)
+      hl.WinSeparator = { fg = "#4b6f9e", bg = colors.bg_dark }
+      hl.CopilotSuggestion = { fg = "#4b6f9e", bg = colors.bg_dark }
     end,
   },
 }
