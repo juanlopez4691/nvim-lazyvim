f75e183f0652ca43c6389be8781875eeb173e3b7
Joan López <juanlopez4691@gmail.com>
2025-01-04 13:55:15 +0100

    fix: remove conflicting keymaps for buffer navigation

--- lua/config/keys/buffers.lua
+++ lua/config/keys/buffers.lua
@@ -1,5 +1,9 @@
 local keymap = vim.keymap
 
+-- Buffer navigation keymaps conflict with jump to screen top and bottom
+keymap.del("n", "<S-h>")
+keymap.del("n", "<S-l>")
+
 -- Redefine buffer explorer keymap
 keymap.del("n", "<leader>be")
 keymap.set("n", "<leader>be", ":Neotree buffers toggle=true<cr>", { desc = "Buffer Explorer", silent = true })
