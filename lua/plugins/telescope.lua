c6c35d87dbe4b2846d4bea72639d4feeb0d2aa5d
Joan López <juanlopez4691@gmail.com>
2025-01-07 20:29:51 +0100

    refactor: load telescope extensions
    Refactored loading of telescope extensions telescope-project and
    telescope-file-browser attending to what's suggested in section "19.6.
    Complex Plugin Example: Telescope-live-grep-args", "Chapter 19.
    Comprehensive Guide to LazyVim Configuration" from book "LazyVim for
    Ambitious Developers". See https://shorturl.at/AoBuD

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,53 +1,86 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  opts = {
-    defaults = {
-      layout_config = {
-        height = 0.95,
-        width = 0.95,
+  { "nvim-telescope/telescope-file-browser.nvim" },
+  { "nvim-telescope/telescope-project.nvim" },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-file-browser.nvim",
+        config = function()
+          LazyVim.on_load("telescope", function()
+            require("telescope").load_extension("file_browser")
+          end)
+        end,
       },
-      mappings = {
-        i = {
-          ["<C-j>"] = require("telescope.actions").move_selection_next,
-          ["<C-k>"] = require("telescope.actions").move_selection_previous,
-          ["<C-h>"] = require("telescope.actions").preview_scrolling_left,
-          ["<C-l>"] = require("telescope.actions").preview_scrolling_right,
-        },
-        n = {
-          ["j"] = require("telescope.actions").move_selection_next,
-          ["k"] = require("telescope.actions").move_selection_previous,
-          -- Add more custom normal mode mappings here
-        },
+      {
+        "nvim-telescope/telescope-project.nvim",
+        config = function()
+          LazyVim.on_load("telescope", function()
+            require("telescope").load_extension("project")
+          end)
+        end,
       },
     },
-  },
-  keys = {
-    {
-      "<leader>fp",
-      function()
-        local telescope = require("telescope")
-        local telescope_custom = require("helpers.telescope")
+    opts = {
+      defaults = {
+        layout_config = {
+          height = 0.95,
+          width = 0.95,
+        },
+        mappings = {
+          i = {
+            ["<C-j>"] = require("telescope.actions").move_selection_next,
+            ["<C-k>"] = require("telescope.actions").move_selection_previous,
+            ["<C-h>"] = require("telescope.actions").preview_scrolling_left,
+            ["<C-l>"] = require("telescope.actions").preview_scrolling_right,
+          },
+          n = {
+            ["j"] = require("telescope.actions").move_selection_next,
+            ["k"] = require("telescope.actions").move_selection_previous,
+            -- Add more custom normal mode mappings here
+          },
+        },
+      },
+      keys = {
+        {
+          "<leader>fp",
+          function()
+            local telescope = require("telescope")
+            local telescope_custom = require("helpers.telescope")
+            local custom_theme = telescope_custom.get_dropdown_custom(0.7, 0.5)
 
-        local custom_theme = telescope_custom.get_dropdown_custom(0.5, 0.5)
-        telescope.extensions.project.project(vim.tbl_deep_extend("force", custom_theme, {
-          display_type = "full",
-        }))
-      end,
-      desc = "Find project",
-    },
-  },
-  extensions = {
-    project = {
-      hidden_files = true,
-      order_by = "asc",
-      search_by = "title",
-    },
-    file_browser = {
-      hijack_netrw = true,
-      git_status = true,
-      grouped = true,
-      prompt_path = true,
-      hidden = { file_browser = true, folder_browser = true },
+            telescope.extensions.project.project(vim.tbl_deep_extend("force", custom_theme, {
+              display_type = "full",
+            }))
+          end,
+          desc = "Find project",
+        },
+      },
+      extensions = {
+        project = {
+          hidden_files = true,
+          order_by = "asc",
+          search_by = "title",
+          mappings = {
+            ["b"] = function()
+              local ok, telescope = pcall(require, "telescope")
+              if not ok then
+                return
+              end
+              telescope.extensions.file_browser.file_browser({
+                grouped = true,
+              })
+            end,
+          },
+        },
+        file_browser = {
+          hijack_netrw = true,
+          git_status = true,
+          grouped = true,
+          prompt_path = true,
+          hidden = { file_browser = true, folder_browser = true },
+        },
+      },
     },
   },
 }
