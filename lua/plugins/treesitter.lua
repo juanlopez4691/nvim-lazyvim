4c11d6afaf697bd60bee4729f0dd7bb33320e0ae
Joan López <juanlopez4691@gmail.com>
2025-01-11 16:10:01 +0100

    perf: improve loading time of plugin nvim-treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,45 +1,47 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
+  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+  event = { "LazyFile", "VeryLazy" },
+  opts = function(_, opts)
+    opts.ensure_installed = opts.ensure_installed or {}
+    vim.list_extend(opts.ensure_installed, {
+      "blade",
       "css",
       "http",
       "markdown",
       "php",
+      "php_only",
       "phpdoc",
       "vue",
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "blade",
-        "css",
-        "php",
-        "php_only",
-        "phpdoc",
-      },
-    },
-    config = function(_, opts)
-      ---@class ParserInfo
+    })
+    return opts
+  end,
+  config = function(_, opts)
+    if type(opts.ensure_installed) == "table" then
+      opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
+    end
+
+    require("nvim-treesitter.configs").setup(opts)
+
+    -- Defer Blade parser configuration
+    vim.defer_fn(function()
       local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.blade = {
-        install_info = {
-          url = "https://github.com/EmranMR/tree-sitter-blade",
-          files = { "src/parser.c" },
-          branch = "main",
-        },
-        filetype = "blade",
-      }
+      if not parser_config.blade then
+        parser_config.blade = {
+          install_info = {
+            url = "https://github.com/EmranMR/tree-sitter-blade",
+            files = { "src/parser.c" },
+            branch = "main",
+          },
+          filetype = "blade",
+        }
 
-      vim.filetype.add({
-        pattern = {
-          [".*%.blade%.php"] = "blade",
-        },
-      })
-
-      require("nvim-treesitter.configs").setup(opts)
-    end,
-  },
+        vim.filetype.add({
+          pattern = {
+            [".*%.blade%.php"] = "blade",
+          },
+        })
+      end
+    end, 100)
+  end,
 }
