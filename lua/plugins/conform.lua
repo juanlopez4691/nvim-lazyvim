b91d63f0e1f841ab8979c400a4ad3df45e0e9e97
Joan López <juanlopez4691@gmail.com>
2025-01-22 12:18:31 +0100

    feat: integrate pint as linter and formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,5 +6,20 @@ return {
       php = { "pint", "phpcbf", stop_after_first = true },
       python = { "isort", "black", stop_after_first = true },
     },
+    formatters = {
+      injected = { options = { ignore_errors = true } },
+      pint = {
+        meta = {
+          url = "https://github.com/laravel/pint",
+          description = "Laravel Pint is an opinionated PHP code style fixer for minimalists. Pint is built on top of PHP-CS-Fixer and makes it simple to ensure that your code style stays clean and consistent.",
+        },
+        command = require("conform.util").find_executable({
+          vim.fn.stdpath("data") .. "/mason/bin/pint",
+          "vendor/bin/pint",
+        }, "pint"),
+        args = { "$FILENAME" },
+        stdin = false,
+      },
+    },
   },
 }
