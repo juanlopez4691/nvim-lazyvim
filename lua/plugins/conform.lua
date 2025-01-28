3997e841a174124aed9875ecf4bf2b56c3189263
Joan López <juanlopez4691@gmail.com>
2025-01-28 14:07:42 +0100

    fix: overlapping formatters for php code

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,3 +1,21 @@
+--[[
+--- Configure PHP formatters dynamically based on project setup.
+---
+--- Prevents phpcbf from being used if the project is using pint.
+---
+--- @return table Linter names for PHP files
+---]]
+local function get_php_formatters()
+  local fs = require("helpers.filesystem")
+  local project_root = vim.fn.getcwd()
+
+  if fs.file_exists(project_root .. "/pint.json") and fs.file_exists(project_root .. "/vendor/bin/pint") then
+    return { "pint" }
+  else
+    return { "phpcbf" }
+  end
+end
+
 return {
   "stevearc/conform.nvim",
   lazy = true,
@@ -8,7 +26,10 @@ return {
     return vim.tbl_deep_extend("force", opts or {}, {
       formatters_by_ft = {
         javascript = { "prettierd", "prettier", stop_after_first = true },
-        php = { "pint", "phpcbf", stop_after_first = true },
+        php = function()
+          return vim.tbl_deep_extend("force", opts.formatters_by_ft or {}, get_php_formatters())
+        end,
+        -- php = { "pint", "phpcbf", stop_after_first = true },
         python = { "isort", "black", stop_after_first = true },
       },
       formatters = {
