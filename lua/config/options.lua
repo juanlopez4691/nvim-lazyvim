072b8066fd377d275a89f485447984c90397e644
Joan López <juanlopez4691@gmail.com>
2025-02-18 19:32:06 +0100

    fix: python provider

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,17 @@
 
 vim.g.lazyvim_php_lsp = "intelephense"
 
+if vim.fn.executable("pyenv") == 1 then
+  local pyenv_python = vim.fn.trim(vim.fn.system("pyenv which python"))
+  if vim.v.shell_error == 0 and vim.fn.filereadable(pyenv_python) == 1 then
+    vim.g.python3_host_prog = pyenv_python
+  else
+    vim.g.python3_host_prog = "/opt/homebrew/opt/python@3.13/bin/python3"
+  end
+else
+  vim.g.python3_host_prog = "/opt/homebrew/opt/python@3.13/bin/python3"
+end
+
 vim.g.root_spec = {
   "lsp",
   { ".git", "composer.json" },
