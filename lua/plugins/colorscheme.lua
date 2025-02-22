03a0eb6489ba212f8a0baf5002d6d57a55b18707
Joan López <juanlopez4691@gmail.com>
2025-02-22 22:08:55 +0100

    feat: add custom config for colorscheme catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,31 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = true,
-  opts = {
-    style = "night",
-    dim_inactive = true,
-    lualine_bold = true,
-    on_highlights = function(hl, colors)
-      hl.WinSeparator = { fg = "#4b6f9e", bg = colors.bg_dark }
-      hl.CopilotSuggestion = { fg = "#4b6f9e", bg = colors.bg_dark }
-    end,
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = true,
+    opts = {
+      highlight_overrides = {
+        all = function()
+          return {
+            WinSeparator = { fg = "#4b6f9e" },
+            CopilotSuggestion = { fg = "#4b6f9e" },
+            SnacksIndentScope = { fg = "#5e5e87" },
+          }
+        end,
+      },
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      style = "night",
+      dim_inactive = true,
+      lualine_bold = true,
+      on_highlights = function(hl, colors)
+        hl.WinSeparator = { fg = "#4b6f9e", bg = colors.bg_dark }
+        hl.CopilotSuggestion = { fg = "#4b6f9e", bg = colors.bg_dark }
+      end,
+    },
   },
 }
