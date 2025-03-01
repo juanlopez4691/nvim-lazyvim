a9e818bc94f2ef6e64ea56dddba3a592d6a47b86
Joan López <juanlopez4691@gmail.com>
2025-03-01 23:49:00 +0100

    style: dashboard config

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -22,33 +22,49 @@ return {
           return ("Good %s, %s"):format(day_part, username)
         end
 
-        -- stylua: ignore
         return {
           { padding = 0, align = "center", text = { header, hl = "header" } },
           { padding = 2, align = "center", text = { greeting(), hl = "header" } },
-          { title = "Builtin Actions", indent = 2, padding = 1,
           {
-            icon = " ",
-            key = "p",
-            desc = "Find Project",
-            action = function()
-              local project_base_path = vim.fn.expand("~/Projects")
-              Snacks.picker.projects({
-                dev = { project_base_path, project_base_path .. "/Sites/personal/", project_base_path .. "/Sites/somoscuatro/" },
-              })
-            end,
-          },
-            { icon = " ", key = "f", desc = "Find File",       action = ":lua Snacks.dashboard.pick('files')" },
-            { icon = " ", key = "n", desc = "New File",        action = ":ene | startinsert" },
+            title = "Builtin Actions",
+            indent = 2,
+            padding = 1,
+            {
+              icon = " ",
+              key = "p",
+              desc = "Find Project",
+              action = function()
+                local project_base_path = vim.fn.expand("~/Projects")
+                Snacks.picker.projects({
+                  dev = {
+                    project_base_path,
+                    project_base_path .. "/Sites/personal/",
+                    project_base_path .. "/Sites/somoscuatro/",
+                  },
+                })
+              end,
+            },
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
             { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
             { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
             { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-            { icon = " ", key = "q", desc = "Quit",            action = ":qa" } },
-          { title = "Maintenance Actions", indent = 2, padding = 2,
-            { icon = " ", key = "c", desc = "Config",      action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})", },
-            { icon = "󰒲 ", key = "l", desc = "Lazy",        action = ":Lazy" },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+          {
+            title = "Maintenance Actions",
+            indent = 2,
+            padding = 2,
+            {
+              icon = " ",
+              key = "c",
+              desc = "Config",
+              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+            },
+            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
             { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
-            { icon = "󱁤 ", key = "m", desc = "Mason",       action = ":Mason" },                          },
+            { icon = "󱁤 ", key = "m", desc = "Mason", action = ":Mason" },
+          },
           { section = "startup" },
         }
       end,
