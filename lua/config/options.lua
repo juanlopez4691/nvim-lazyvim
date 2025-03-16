b262e63f3eeb8e1312282eb9055a313c478415b3
Joan López <juanlopez4691@gmail.com>
2025-03-16 11:03:04 +0100

    feat: use default clipboard for yank/paste

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Use default clipboard for yank/paste
+vim.opt.clipboard = ""
 
 -- intelephense as PHP LSP
 vim.g.lazyvim_php_lsp = "intelephense"
