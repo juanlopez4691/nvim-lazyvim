3eb2d5212b800fd950ca1ab061d4bf27156c906a
Joan López <juanlopez4691@gmail.com>
2025-03-23 19:09:39 +0100

    feat: setup avante with Google Gemini, model gemini-2.0-flash-thinking-exp-01-21

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -3,6 +3,22 @@ return {
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
   opts = {
+    provider = "gemini",
+    gemini = {
+      -- @see https://ai.google.dev/gemini-api/docs/models/gemini
+      model = "gemini-2.0-flash-thinking-exp-01-21",
+      -- model = "gemini-1.5-pro-exp-0827",
+      -- model = "gemini-1.5-flash",
+      temperature = 0,
+      max_tokens = 8192,
+    },
+    behaviour = {
+      auto_suggestions = false, -- Experimental stage
+      auto_set_highlight_group = true,
+      auto_set_keymaps = true,
+      auto_apply_diff_after_generation = false,
+      support_paste_from_clipboard = true,
+    },
     file_selector = {
       provider = "snacks",
       -- Options override for custom providers
