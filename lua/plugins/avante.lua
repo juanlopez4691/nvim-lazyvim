5d8e9d2b046dbb19c330117d25f9b09dca986106
Joan López <juanlopez4691@gmail.com>
2025-03-28 12:59:03 +0100

    refactor(avante): plugin config

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -2,6 +2,25 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "folke/snacks.nvim",
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      "MeanderingProgrammer/render-markdown.nvim",
+      lazy = true,
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
   opts = {
     provider = "gemini",
     gemini = {
@@ -24,25 +43,6 @@ return {
       -- Options override for custom providers
       provider_opts = {},
     },
-  },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "folke/snacks.nvim",
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      "MeanderingProgrammer/render-markdown.nvim",
-      lazy = true,
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
     },
   },
 }
