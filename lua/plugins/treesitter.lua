ea967e8e57690036baaef37fcd7c08125c9bc1d1
Joan López <juanlopez4691@gmail.com>
2025-05-08 14:41:46 +0200

    refactor: treesitter configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    opts.ensure_installed = opts.ensure_installed or {}
-    vim.list_extend(opts.ensure_installed, {
+  opts = {
+    ensure_installed = {
       "blade",
       "css",
       "http",
@@ -11,9 +10,8 @@ return {
       "php_only",
       "phpdoc",
       "vue",
-    })
-    return opts
-  end,
+    },
+  },
   config = function(_, opts)
     if type(opts.ensure_installed) == "table" then
       opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
