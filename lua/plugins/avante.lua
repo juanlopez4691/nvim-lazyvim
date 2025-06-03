5b864e8d30ab751f4bdbd7f2af0a766b9ad2b3de
Joan López <juanlopez4691@gmail.com>
2025-06-03 13:11:57 +0200

    fix: deprecated config for plugin avante.nvim

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -23,27 +23,31 @@ return {
   },
   opts = {
     provider = "gemini",
-    gemini = {
-      -- @see https://ai.google.dev/gemini-api/docs/models/gemini
-      model = "gemini/gemini-2.5-flash-preview-04-17",
-      -- model = "gemini-2.0-flash-thinking-exp-01-21",
-      temperature = 0,
-      max_tokens = 8192,
-    },
-    behaviour = {
-      auto_suggestions = false, -- Experimental stage
-      auto_set_highlight_group = true,
-      auto_set_keymaps = true,
-      auto_apply_diff_after_generation = false,
-      support_paste_from_clipboard = true,
-    },
-    file_selector = {
-      provider = "snacks",
-      -- Options override for custom providers
-      provider_opts = {},
-    },
-    web_search_engine = {
-      provider = "brave", -- tavily, serpapi, searchapi, google or kagi
+    providers = {
+      gemini = {
+        -- @see https://ai.google.dev/gemini-api/docs/models/gemini
+        model = "gemini/gemini-2.5-flash-preview-04-17",
+        -- model = "gemini-2.0-flash-thinking-exp-01-21",
+        extra_request_body = {
+          temperature = 0,
+          max_tokens = 8192,
+        },
+      },
+      behaviour = {
+        auto_suggestions = false, -- Experimental stage
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = true,
+      },
+      file_selector = {
+        provider = "snacks",
+        -- Options override for custom providers
+        provider_opts = {},
+      },
+      web_search_engine = {
+        provider = "brave", -- tavily, serpapi, searchapi, google or kagi
+      },
     },
   },
 }
