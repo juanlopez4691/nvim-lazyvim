c0fedc303f7ab2e7603fc7ceccfef77bd9d733a8
Joan López <juanlopez4691@gmail.com>
2025-08-03 23:52:36 +0200

    feat: hide copilot suggestions if autocompletion menu is visible

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -132,3 +132,19 @@ vim.api.nvim_create_autocmd("InsertLeave", {
   end,
   group = groupDiagnostics,
 })
+
+-- Hide copilot suggestions if autocompletion menu is visible
+vim.api.nvim_create_autocmd("User", {
+  pattern = "BlinkCmpMenuOpen",
+  callback = function()
+    vim.b.copilot_suggestion_hidden = true
+  end,
+})
+
+-- Show copilot suggestions if autocompletion menu is not visible
+vim.api.nvim_create_autocmd("User", {
+  pattern = "BlinkCmpMenuClose",
+  callback = function()
+    vim.b.copilot_suggestion_hidden = false
+  end,
+})
