6d65c378c6ebe799d391fa52d76341b23e8323d2
Joan López <juanlopez4691@gmail.com>
2025-10-14 11:19:16 +0200

    feat: add plugin avante.nvim and dependencies

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,11 +1,32 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  opts = {
-    ft = {
-      "markdown",
-      "norg",
-      "rmd",
-      "org",
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    enabled = false,
+  },
+  {
+    "OXY2DEV/markview.nvim",
+    event = "VeryLazy",
+    -- lazy = false,
+    opts = {
+      preview = {
+        enable = true,
+        enable_hybrid_mode = true,
+        icon_provider = "mini",
+        filetypes = { "Avante", "markdown", "md", "rmd", "quarto" },
+        ignore_buftypes = {},
+      },
     },
+    config = function(_, opts)
+      local markview = require("markview")
+      local presets = require("markview.presets")
+
+      markview.setup(vim.tbl_deep_extend("force", opts or {}, {
+        markdown = {
+          headings = presets.marker,
+          tables = presets.simple,
+          horizonal_rules = presets.thin,
+        },
+      }))
+    end,
   },
 }
