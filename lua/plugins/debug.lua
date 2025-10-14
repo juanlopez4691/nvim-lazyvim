139e93fc11b2d0cdf20e15242ce51c6434b12ff0
Joan López <juanlopez4691@gmail.com>
2025-10-14 12:15:43 +0200

    feat: ensure dap is set up to debug PHP

---
+++ lua/plugins/debug.lua
@@ -0,0 +1,22 @@
+return {
+  "mfussenegger/nvim-dap",
+  opts = function(_, opts)
+    local dap = require("dap")
+
+    if not dap.configurations.php and vim.fn.filereadable(".vscode/launch.json") == 0 then
+      dap.configurations.php = {
+        {
+          type = "php",
+          request = "launch",
+          name = "Listen for Xdebug",
+          port = 9003,
+          pathMappings = {
+            ["/var/www/html"] = "${workspaceFolder}",
+          },
+        },
+      }
+    end
+
+    return opts
+  end,
+}
