bc1b6bdb0938963c18162e87d6975b79a7043720
Joan López <juanlopez4691@gmail.com>
2025-10-19 15:03:21 +0200

    feat: updated must-have treesitter parsers

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "blade",
+      "css",
+      "go",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "php",
+      "php_doc",
+      "php_only",
+      "typescript",
+      "vue",
+      "yaml",
+    },
+  },
+}
