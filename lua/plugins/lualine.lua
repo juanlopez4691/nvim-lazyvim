e91a5e45cd0699ae0e0dca9112c9ecb0d187c1fd
Joan López <juanlopez4691@gmail.com>
2025-10-21 10:37:35 +0200

    feat: display LSP clients in status line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,35 @@
+-- Display attached LSP clients in the status line
+local function lsp_clients()
+  local clients = vim.lsp.get_clients({ bufnr = 0 })
+  if #clients == 0 then
+    return "No LSPs"
+  end
+  return table.concat(
+    vim.tbl_map(function(client)
+      local icons = {
+        lua_ls = "",
+        pyright = "",
+        tsserver = "",
+        gopls = "",
+        rust_analyzer = "",
+        clangd = "",
+        intelephense = "",
+        jsonls = "",
+        tailwindcss = "󱏿",
+        dockerls = "",
+        taplo = "",
+        marksman = "",
+        vtsls = "",
+        cssls = "",
+        html = "",
+      }
+      local icon = icons[client.name] or ""
+      return string.format("%s %s", icon, client.name)
+    end, clients),
+    ", "
+  )
+end
+
 local filename = {
   "filename",
   path = 4,
@@ -29,6 +61,9 @@ return {
         LazyVim.lualine.root_dir(),
         { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
         { LazyVim.lualine.pretty_path() },
+      },
+      lualine_x = {
+        { lsp_clients, color = { fg = "#7aa2f7" } }, -- Subtle bluish color
       },
     },
     winbar = {
