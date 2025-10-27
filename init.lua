7152933c5d2a48069f2c2377b3c3e4422abfe7ef
Joan López <juanlopez4691@gmail.com>
2025-10-27 09:28:16 +0100

    feat: remove deferred activation of syntax highlighting

--- init.lua
+++ init.lua
@@ -3,10 +3,5 @@ if vim.loader then
   vim.loader.enable()
 end
 
-vim.cmd("syntax off")
-vim.defer_fn(function()
-  vim.cmd("syntax on")
-end, 50)
-
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
