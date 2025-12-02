8d7d6fa2530b63076c809242f216095bce213604
Joan López <juanlopez4691@gmail.com>
2025-12-02 10:49:54 +0100

    feat: add plugin ravitemer/mcphub.nvim

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,6 +1,32 @@
 return {
-  "yetone/avante.nvim",
-  opts = {
-    web_search_engine = "Brave",
+  {
+    "yetone/avante.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "ravitemer/mcphub.nvim",
+        build = "npm install -g mcp-hub@latest",
+        config = function()
+          require("mcphub").setup()
+        end,
+      },
+    },
+    opts = {
+      web_search_engine = "Brave",
+      extensions = {
+        avante = {
+          make_slash_commands = true,
+        },
+      },
+      system_prompt = function()
+        local hub = require("mcphub").get_hub_instance()
+        return hub and hub:get_active_servers_prompt() or ""
+      end,
+      custom_tools = function()
+        return {
+          require("mcphub.extensions.avante").mcp_tool(),
+        }
+      end,
+    },
   },
 }
