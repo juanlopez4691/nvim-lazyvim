0b32dcd2351d1d491db7cf7b3294f8dcbc8c8951
Joan López <juanlopez4691@gmail.com>
2026-01-06 19:55:07 +0100

    feat: hide statusline on starter page

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -58,14 +58,35 @@ local disabled_filetypes = {
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
-  -- Refresh lualine when macro recording starts/stops
   init = function()
+    -- Restore LazyVim default: hide statusline on starter page
+    vim.g.lualine_laststatus = vim.o.laststatus
+
+    if vim.fn.argc(-1) > 0 then
+      -- set an empty statusline till lualine loads
+      vim.o.statusline = " "
+    else
+      -- hide the statusline on the starter page
+      vim.o.laststatus = 0
+    end
+
+    -- Restore laststatus after lazy plugins initialized
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "VeryLazy",
+      once = true,
+      callback = function()
+        vim.o.laststatus = vim.g.lualine_laststatus
+      end,
+    })
+
+    -- Refresh lualine when macro recording starts/stops
     local function refresh()
       local ok, lualine = pcall(require, "lualine")
       if ok then
         lualine.refresh({ place = { "statusline" } })
       end
     end
+
     vim.api.nvim_create_autocmd("RecordingEnter", { callback = refresh })
     vim.api.nvim_create_autocmd("RecordingLeave", {
       callback = function()
