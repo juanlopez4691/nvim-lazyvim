98a63efa31335ea33d06a541e9b1cf7d32b86675
Joan López <juanlopez4691@gmail.com>
2026-02-11 16:27:53 +0100

    feat: add plugin sindrets/diffview.nvim

---
+++ lua/plugins/git.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      {
+        "dv",
+        function()
+          if next(require("diffview.lib").views) == nil then
+            vim.cmd("DiffviewOpen")
+          else
+            vim.cmd("DiffviewClose")
+          end
+        end,
+        desc = "Toggle Diffview window",
+      },
+    },
+    opts = {},
+  },
+}
