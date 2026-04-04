bbca9824b456ed1a89a7b60d18267e0f799de926
Joan López <juanlopez4691@gmail.com>
2026-04-04 16:15:51 +0200

    perf: set defaults.lazy=true to defer plugin loading

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,9 +25,9 @@ require("lazy").setup({
     { import = "plugins" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
+    -- By default, all plugins will be lazy-loaded. LazyVim plugins are already configured with proper
+    -- loading events. Custom plugins should specify event/cmd/keys triggers for deferred loading.
+    lazy = true,
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
     -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
