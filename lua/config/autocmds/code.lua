b7cbe7af18c2a79cbddff1eb159b11c821e9bf41
Joan López <juanlopez4691@gmail.com>
2026-07-06 22:30:56 +0200

    feat: add group to code related autocommands

--- lua/config/autocmds/code.lua
+++ lua/config/autocmds/code.lua
@@ -1,5 +1,8 @@
+local group = vim.api.nvim_create_augroup("code", { clear = true })
+
 -- Add $ to iskeyword for some filetypes
 vim.api.nvim_create_autocmd("FileType", {
+  group = group,
   pattern = { "php", "javascript", "typescript" },
   callback = function()
     vim.opt_local.iskeyword:append("$")
