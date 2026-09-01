fc8a0d2995f5e9973ee77f73209dbc3db10ec9dc
Joan López <juanlopez4691@gmail.com>
2026-09-01 17:11:14 +0200

    refactor: drop single_file_support, dead under vim.lsp.config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -87,7 +87,6 @@ return {
       },
       antlersls = {
         filetypes = { "antlers", "html" },
-        single_file_support = true,
         root_dir = function(bufnr, on_dir)
           on_dir(vim.fs.root(bufnr, { "composer.json", ".git" }) or vim.fs.dirname(vim.api.nvim_buf_get_name(bufnr)))
         end,
