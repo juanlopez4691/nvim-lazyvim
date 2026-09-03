52c142fe57706d96de8c6b7bea9349bf7f5ae1ce
Joan López <juanlopez4691@gmail.com>
2026-09-03 11:22:14 +0200

    refactor: plugin tiny-inline-diagnostics config

--- lua/plugins/inline-diagnostics.lua
+++ lua/plugins/inline-diagnostics.lua
@@ -1,20 +1,22 @@
 return {
   "rachartier/tiny-inline-diagnostic.nvim",
   event = "VeryLazy",
-  opts = {
-    preset = "powerline",
-    options = {
-      show_all_diags_on_cursorline = true,
-      use_icons_from_diagnostic = true,
-      add_messages = {
-        messages = true,
-        show_multiple_glyphs = false,
+  config = function()
+    require("tiny-inline-diagnostic").setup({
+      preset = "powerline",
+      options = {
+        show_all_diags_on_cursorline = true,
+        use_icons_from_diagnostic = true,
+        add_messages = {
+          messages = true,
+          show_multiple_glyphs = false,
+        },
+        multilines = {
+          enabled = true,
+          trim_whitespace = true,
+          always_show = true,
+        },
       },
-      multilines = {
-        enabled = true,
-        trim_whitespace = true,
-        always_show = true,
-      },
-    },
-  },
+    })
+  end,
 }
